61f2e5d021d55840517b7a4c9f932ae9981d4e17
RichardMizak <72248474+RichardMizak@users.noreply.github.com>
2021-02-11 10:28:27 +0100

    Update Database.java

--- ChatApp/Database.java
+++ ChatApp/Database.java
@@ -1,25 +1,80 @@
 package sk.kosickaakademia.app;
 
-import java.util.ArrayList;
+import java.sql.*;
 
 public class Database {
-        private String url="jdbc:mysql://localhost:3306/world_x";
-        private String username="root";
-        private String password="";
+        private String url="jdbc:mysql://itsovy.sk:3306/chat2021";
+        private String username="mysqluser";
+        private String password="Kosice2021!";
+        private final String insertNewUser="INSERT INTO user (login, password) VALUES (?, ?) ";
+        private final String loginUser="SELECT * FROM user WHERE login LIKE ? AND password LIKE ? ";
 
-        ArrayList<Users> registeredUsers;
-        ArrayList<Users> loggedUsers;
-
-        public void registerAndLogin() {
-            loggedUsers = new ArrayList<Users>();
-            registeredUsers = new ArrayList<Users>();
+        private Connection getConnection() throws ClassNotFoundException, SQLException {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection conn= DriverManager.getConnection(url,username,password);
+            return conn;
         }
-            public void register(String username,String password){
-                Users registeredUsers=new Users(username,password);
 
-            }
-            public void login(String username,String password){
-                Users loggedUsers=new Users(username,password);
+        public boolean insertNewUser(String login, String password){
+            if(login==null || login.equals("") || password==null || password.length()<6)
+                return false;
 
+            String hpwd= new Util().MD5(password);
+            try{
+                Connection conn=getConnection();
+                if (conn==null){
+                    System.out.println("error");
+                    return false;
+                }else{
+                    PreparedStatement ps=conn.prepareStatement(insertNewUser);
+                    ps.setString(1,login);
+                    ps.setString(2, hpwd);
+                    int result=ps.executeUpdate();
+                    conn.close();
+                    if (result==0)
+                        return false;
+                    else {
+                        return true;
+                    }
+                }
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
             }
+
+            return true;
+        }
+        public Users loginUser(String login,String password){
+            if(login==null || login.equals("") || password==null || password.length()<6)
+                return null;
+            String hpwd=new Util().MD5(password);
+            try {
+                Connection conn=getConnection();
+                if (conn==null){
+                    System.out.println("error");
+                    return null;
+                }
+                PreparedStatement ps= conn.prepareStatement(loginUser);
+                ps.setString(1, login);
+                ps.setString(2, hpwd);
+                ResultSet rs=ps.executeQuery();
+                if(rs.next()){
+                    System.out.println("logged");
+                    int id=rs.getInt("id");
+                    Users user=new Users(login, hpwd, id);
+                    conn.close();
+                    return user;
+                }else{
+                    conn.close();
+                    return null;
+                }
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
 }
