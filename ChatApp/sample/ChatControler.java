004069cc22db6fc6a806d253247498381da6d482
RichardMizak <72248474+RichardMizak@users.noreply.github.com>
2021-02-15 18:36:22 +0100

    Create ChatControler.java

---
+++ ChatApp/sample/ChatControler.java
@@ -0,0 +1,14 @@
+package sample;
+
+import javafx.scene.control.Label;
+import sk.kosickaakademia.app.Users;
+
+public class ChatControler {
+    public Label lbl_login;
+    private Users users;
+
+    public ChatControler(){
+
+    }
+
+}
