a8012fc4986895f97c659a4499dff51431a67ef6
RichardMizak <72248474+RichardMizak@users.noreply.github.com>
2021-02-18 20:48:05 +0100

    Update Controler.java

--- ChatApp/sample/Controler.java
+++ ChatApp/sample/Controler.java
@@ -16,21 +16,21 @@ public class Controller {
 
     @FXML
     private TextField txt_login;
-    public PasswordField txt_password;
+    public PasswordField txt_pwd;
     public Label lbl_error;
     public Button btn_login;
 //-----------------------------------------------------------------------------------
-    public void btn_click(ActionEvent actionEvent) {
-        System.out.println("Hello! It works!");
+    public void btn_click(ActionEvent ae) {
+        System.out.println("It works.");
         String login = txt_login.getText().trim();
-        String password = txt_password.getText().trim();
+        String password = txt_pwd.getText().trim();
         if(login.length()>0 && password.length()>0){
             Database db = new Database();
             Users users = db.loginUser(login,password);
             if(users ==null){
                 lbl_error.setVisible(true);
             }else{
-                System.out.println("You are logged!");
+                System.out.println("You are logged.");
                 openMainForm(users);
             }
         }
@@ -38,7 +38,6 @@ public class Controller {
 //---------------------------------------------------------------------------
     private void openMainForm(Users users) {
         try {
-            //FXMLLoader root = new FXMLLoader().load(getClass().getResource("Chat.fxml"));
             FXMLLoader root = new FXMLLoader();
             root.setLocation(getClass().getResource("Chat.fxml"));
             Stage stage = new Stage();
@@ -46,7 +45,7 @@ public class Controller {
             stage.setScene(new Scene(root.load(), 800, 600));
             stage.show();
             ChatController mc= root.getController();
-          //  mc.setUser(user);
+            mc.setUser(users);
             mc.initLoginName();
             btn_login.getScene().getWindow().hide();
         }catch(Exception e){
